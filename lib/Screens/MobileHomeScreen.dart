66492393ad7815a31bec48e2642a4a2160c9b961
Graham Williams <Graham.Williams@togaware.com>
2021-06-14 15:07:53 +1000

    Remove bottom "Start a room" and list button.

--- lib/Screens/MobileHomeScreen.dart
+++ lib/Screens/MobileHomeScreen.dart
@@ -1,6 +1,6 @@
-import 'package:clubhouse/Modules/Data.dart';
-import 'package:clubhouse/Modules/Room.dart';
-import 'package:clubhouse/Widgets/RoomContainer.dart';
+import 'package:mlflutter/Modules/Data.dart';
+import 'package:mlflutter/Modules/Room.dart';
+import 'package:mlflutter/Widgets/RoomContainer.dart';
 import 'package:flutter/material.dart';
 
 class MobileHomeScreen extends StatefulWidget {
@@ -42,60 +42,6 @@ class _MobileHomeScreenState extends State<MobileHomeScreen> {
                   color: KClubhouseColor.withOpacity(0.8),
                   blurRadius: 1,
                   offset: Offset(0, 25),
-                )
-              ],
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(width: 45),
-                Container(
-                  height: 50,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.green[600],
-                    child: MaterialButton(
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Icon(
-                            Icons.add,
-                            color: Colors.white,
-                          ),
-                          Text(
-                            'Start a room',
-                            style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Stack(
-                  children: [
-                    Icon(
-                      Icons.view_list,
-                      size: 45,
-                    ),
-                    Positioned(
-                      right: 0,
-                      bottom: 5,
-                      child: Icon(
-                        Icons.circle,
-                        size: 20,
-                        color: Colors.green[600],
-                      ),
-                    ),
-                  ],
                 )
               ],
             ),
