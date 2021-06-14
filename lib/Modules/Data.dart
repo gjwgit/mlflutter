fe9729a6955d01433ece16df0e777a51016ea657
Graham Williams <Graham.Williams@togaware.com>
2021-06-14 17:10:41 +1000

    Add more packages.

--- lib/Modules/Data.dart
+++ lib/Modules/Data.dart
@@ -6,6 +6,10 @@ const Color KClubhouseColor = Color(0xfff1efe4);
 
 final List<Member> available = [
   Member(
+    name: 'audit',
+    profilePicture: 'assets/c1.jpg',
+  ),
+  Member(
     name: 'azcv',
     profilePicture: 'assets/azcv.png',
   ),
@@ -42,15 +46,31 @@ final List<Member> available = [
     profilePicture: 'assets/c2.jpg',
   ),
   Member(
+    name: 'movies',
+    profilePicture: 'assets/c5.jpeg',
+  ),
+  Member(
     name: 'objects',
     profilePicture: 'assets/c2.jpg',
+  ),
+  Member(
+    name: 'ocsvm',
+    profilePicture: 'assets/c5.jpeg',
   ),
   Member(
     name: 'opencv',
     profilePicture: 'assets/c2.jpg',
   ),
   Member(
+    name: 'pyiris',
+    profilePicture: 'assets/c4.jpeg',
+  ),
+  Member(
     name: 'pyspeech',
+    profilePicture: 'assets/c2.jpg',
+  ),
+  Member(
+    name: 'rain',
     profilePicture: 'assets/c2.jpg',
   ),
   Member(
@@ -150,49 +170,37 @@ final List<Room> rooms = [
     ],
   ),
   Room(
-    name: 'Photographers',
+    name: 'Prediction',
     speakers: [
       Member(
-        name: 'Sara',
+        name: 'audit',
         profilePicture: 'assets/c4.jpeg',
       ),
       Member(
-        name: 'Tyler',
+        name: 'iris',
         profilePicture: 'assets/c5.jpeg',
       ),
     ],
     audience: [
       Member(
-        name: 'Rob',
+        name: 'audit',
         profilePicture: 'assets/c1.jpg',
       ),
       Member(
-        name: 'John',
+        name: 'rain',
         profilePicture: 'assets/c2.jpg',
       ),
       Member(
-        name: 'Sara',
+        name: 'pyiris',
         profilePicture: 'assets/c4.jpeg',
       ),
       Member(
-        name: 'Tyler',
+        name: 'movies',
         profilePicture: 'assets/c5.jpeg',
       ),
       Member(
-        name: 'Kal',
+        name: 'ocsvm',
         profilePicture: 'assets/c5.jpeg',
-      ),
-      Member(
-        name: 'Ade',
-        profilePicture: 'assets/c4.jpeg',
-      ),
-      Member(
-        name: 'Minh',
-        profilePicture: 'assets/c2.jpg',
-      ),
-      Member(
-        name: 'Do',
-        profilePicture: 'assets/c1.jpg',
       ),
     ],
   ),
