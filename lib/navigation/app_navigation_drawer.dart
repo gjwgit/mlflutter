efb0ca82586276b0de21341a95efe7db34efea52
Graham Williams <graham.williams@togaware.com>
2025-06-26 08:32:30 +1000

    Rename file to standard.

--- lib/navigation/app_navigation_drawer.dart
+++ lib/navigation/app_navigation_drawer.dart
@@ -19,7 +19,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <https://www.gnu.org/licenses/>.
 ///
-/// Authors: Ting Tang, Arjun Raj
+/// Authors: Ting Tang, Arjun Raj, Graham Williams
 
 library;
 
